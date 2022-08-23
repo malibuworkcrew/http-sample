52715d5f748e30938c526b45e7f37bec70241598
Spencer Wood <spencer.wood@oracle.com>
2022-08-23 00:01:21 -0500

    solved

--- src/main/scala/com/hubspot/http/Main.scala
+++ src/main/scala/com/hubspot/http/Main.scala
@@ -4,12 +4,15 @@ import com.hubspot.http.client.HttpClient
 
 import scala.util.Try
 
+// Can run without arguments to achieve default solution
 object Main {
   def main(args: Array[String]): Unit = {
+    // Grab our host and GET/POST paths here, can be input as arguments for testing
     val host = args.headOption.getOrElse("https://hubspotwood.free.beeceptor.com")
     val getPath = Try(args(1)).getOrElse("get")
     val postPath = Try(args(2)).getOrElse("post")
 
+    // Process GET/POST requests, failure will return as Left and success Right
     new HttpClient(host, getPath, postPath).processGetAndSendPost() match {
       case Left(error) =>
         println(s"Error in execution: ${error.msg}")
